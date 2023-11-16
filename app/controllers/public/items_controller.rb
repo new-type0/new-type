d7533d579914221d2b69b4b77d0e4d8f16d426a3
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-11-16 12:18:38 +0000

    [finish]商品一覧作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,17 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items, @sort = get_items(params)
   end
 
   def show
+  end
+  def get_items(params)
+    return Item.all, 'default' unless params[:latest] || params[:price_high_to_low] || params[:price_low_to_high]
+
+    return Item.latest, 'latest' if params[:latest]
+
+    return Item.price_high_to_low, 'price_high_to_low' if params[:price_high_to_low]
+
+    return Item.price_low_to_high, 'price_low_to_high' if params[:price_low_to_high]
   end
 end
