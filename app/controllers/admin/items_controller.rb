3afe4b0e3fde8b12e919848a1d0e055fcff694fb
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-11-17 11:33:27 +0000

    [finish] 商品新規登録画面途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,48 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render 'edit'
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :tax_included_price, :image)
+  end
+
+  def ensure_current_user  # 選択したitemのユーザー情報とログインしているユーザー情報が違う場合は編集できませんよーっ。
+    item = Item.find(params[:id])
+    if item.user_id != current_user.id
+      redirect_to action: :index
+    end
+  end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
