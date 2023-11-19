6128d25e0e53678e9b8440417bb26afa70537fb3
EC2 Default User <ec2-user@ip-172-31-34-105.ap-northeast-1.compute.internal>
2023-11-19 09:19:01 +0000

    [complete]admin_customer_show

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,6 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum is_active: {active: true, non_active: false}
 end
