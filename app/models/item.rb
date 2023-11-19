d2e5324e04b3c89f9ca00b73eb84d96f745acaff
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-11-19 09:06:32 +0000

    [finish]細かな修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,6 @@ class Item < ApplicationRecord
   has_one_attached :image
   scope :price_high_to_low, -> { order(price: :desc) }
   scope :price_low_to_high, -> { order(price: :asc) }
-  
+
   belongs_to :genre
 end
