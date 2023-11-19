07b755084c9cdadee90ad068f02a13ea228d7e6f
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-11-19 16:05:50 +0000

    [finish]配送画面途中2

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,27 @@
 class Public::AddressesController < ApplicationController
   def index
-    @addresses = Addresses.all
+    @addresses = Address.all
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if current_customer
+      @address.customer_id = current_customer.id
+      @address.save
+      redirect_to edit_public_address_path
+    else
+      render 'index'
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :address, :post_code)
   end
 end
