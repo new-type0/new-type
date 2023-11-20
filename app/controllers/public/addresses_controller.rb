f938982cb53a34e24b0d5a6ddd36d70fea3c42a1
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-11-20 04:40:39 +0000

    [finish]配送画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,4 +1,5 @@
 class Public::AddressesController < ApplicationController
+
   def index
     @addresses = Address.all
     @address = Address.new
@@ -6,18 +7,37 @@ class Public::AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
-    if current_customer
-      @address.customer_id = current_customer.id
+    if @address.customer_id == current_customer.id
       @address.save
-      redirect_to edit_public_address_path
+      @addresses = Address.all
+      redirect_to edit_public_address_path(@address)
     else
+      @addresses = Address.all
       render 'index'
     end
   end
 
+
+
   def edit
     @address = Address.find(params[:id])
   end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to edit_public_address_path(@address), notice: '更新に成功しました'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to public_addresses_path, notice: '削除完了しました'
+  end
+
 
   private
 
